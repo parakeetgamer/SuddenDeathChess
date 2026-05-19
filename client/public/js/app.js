7131e1dd9e9c07c3e8f5d50d355eb34ac5cd34d8
Luke Edwards <edwardsluke557@gmail.com>
2026-05-19 01:29:01 -0700

    Cburnett SVG pieces + bigger board

--- client/public/js/app.js
+++ client/public/js/app.js
@@ -463,11 +463,24 @@ function renderBoard() {
       if (S.chess) {
         const p = S.chess.get(squareName);
         if (p) {
-          const GLYPHS = {wK:'♔',wQ:'♕',wR:'♖',wB:'♗',wN:'♘',wP:'♙',bK:'♚',bQ:'♛',bR:'♜',bB:'♝',bN:'♞',bP:'♟'};
+          const PIECE_IMGS = {
+            wK:'https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg',
+            wQ:'https://upload.wikimedia.org/wikipedia/commons/1/15/Chess_qlt45.svg',
+            wR:'https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg',
+            wB:'https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg',
+            wN:'https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg',
+            wP:'https://upload.wikimedia.org/wikipedia/commons/4/45/Chess_plt45.svg',
+            bK:'https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg',
+            bQ:'https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg',
+            bR:'https://upload.wikimedia.org/wikipedia/commons/f/ff/Chess_rdt45.svg',
+            bB:'https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg',
+            bN:'https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg',
+            bP:'https://upload.wikimedia.org/wikipedia/commons/c/c7/Chess_pdt45.svg',
+          };
           const key2 = (p.color==='w' ? 'w' : 'b') + p.type.toUpperCase();
-          const pe = document.createElement('div');
-          pe.className = 'piece ' + (p.color==='w' ? 'white-piece' : 'black-piece');
-          pe.textContent = GLYPHS[key2];
+          const pe = document.createElement('img');
+          pe.src = PIECE_IMGS[key2];
+          pe.className = 'piece';
           sq.appendChild(pe);
         }
       }
